5eb893c5178febfd0736609a44ac53bf1ebe8f4d
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-12-31 22:45:18 -0600

    Added initial project template

---
+++ Swifty Words/Swifty Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty Words
+//
+//  Created by Akshay Ramesh on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
